3b725a4111dde067969c63958b85d0a917056210
Garrett <garrettcura@gmail.com>
2013-11-18 08:51:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Christmasforvets::Application.config.secret_key_base = '424a56a35a08b3b196e9baba3b040c49685786332409daf1c0404db94db42766d9a2edf6fbaacdd486314850ca15ed0759ad6eee3d36cc936f72ad106134b62a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Christmasforvets::Application.config.session_store :cookie_store, key: '_christmasforvets_session'
